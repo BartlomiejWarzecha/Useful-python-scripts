f807599d91df4e1faaabeb6a3087389cf2f43d2a
bawarzec <bartlomiejwarzecha2@gmail.com>
2023-03-18 15:43:46 +0100

    Added backup option and folder showing for each action

--- imageToPDF.py
+++ imageToPDF.py
@@ -1,7 +1,8 @@
-
 import os
 import glob
 import webbrowser
+import shutil
+
 
 from PIL import Image
 from reportlab.pdfgen import canvas
@@ -9,11 +10,10 @@ from reportlab.lib.pagesizes import letter
 def run():
     # Get the PDF file name from user input
 
-    pdf_name = input("Enter PDF file name: "  + "\n")
+    pdf_name = input("Enter PDF file name: ")
 
-    default_img_dir = "default path is empty - change default path in script file"
+    default_img_dir = "write  default_img_dir"
     
-
     # Set the directory containing the PNG and JPG files
     img_dir = input("Enter image directory path (press enter to use default directory): " + "\n"
                     + default_img_dir  + "\n")
@@ -26,31 +26,55 @@ def run():
         if not os.path.exists(img_dir):
             print("Image directory does not exist!")
             exit()
+            
+    print("opening folder to inspekt files:" + img_dir)
+    webbrowser.open(os.path.realpath(img_dir))
 
     # Get the start and end prefixes from user input
-    start_prefix = input("Enter start prefix (e.g. '080'): "  + "\n")
-    end_prefix = input("Enter end prefix (e.g. '110'): "  + "\n")
-
-    # Get a list of image files in the directory
+    start_prefix = input("Enter start prefix (e.g. '080'): "  + "\n" + "\n write \"start\" to get first position" + "\n")
+    end_prefix = input("Enter end prefix (e.g. '110'): "  + "\n" + "\n write \"end\" to get last position" + "\n" )
+  # Get a list of image files in the directory
     img_files = sorted(glob.glob(os.path.join(img_dir, '*.*')))
 
-    # Find the index of the first file with the start prefix
-    start_index = next((i for i, file in enumerate(img_files) if os.path.basename(file).startswith(start_prefix)), None)
+    # Check if start or end prefix is "start" or "end"
+    if start_prefix == "start":
+        start_index = 0
+    else:
+        start_index = next((i for i, file in enumerate(img_files) if os.path.basename(file).startswith(start_prefix)), None)
 
-    # Find the index of the first file with the end prefix
-    end_index = next((i for i, file in enumerate(img_files) if os.path.basename(file).startswith(end_prefix)), None)
+    if end_prefix == "end":
+        end_index = len(img_files) - 1
+    else:
+        end_index = next((i for i, file in enumerate(img_files) if os.path.basename(file).startswith(end_prefix)), None)
+
+
+    if start_index is None:
+        print("No images with the start prefix were found!")
+        exit()
+
+    if end_index is None: 
+        print("No images with the end prefix were found!")
+        exit()
+
+    if start_index > end_index:
+        print("Start prefix should be before end prefix!")
+        exit()
 
     # Select all files between the start and end indexes
     chosen_img_files = img_files[start_index:end_index]
     chosenFilesNumber = len(chosen_img_files)
 
+    if chosenFilesNumber == 0:
+        print("No images with the selected prefixes were found!")
+        exit()
+
     print("Found " + str(chosenFilesNumber) + " files")
 
-    default_output_dir = 'C:\\Users\\Justyna\\Downloads\\ChomikBox\\Arcs'
+    default_output_dir = 'write default_output_dir'
 
     # Get the output directory from user input
     output_dir = input("Enter output directory path (press enter to use default directory):" + "\n"
-                    + default_output_dir )
+                    + default_output_dir  + "\n" )
     if output_dir == "":
         # If no output directory is provided, use the default directory
         output_dir = default_output_dir
@@ -91,8 +115,26 @@ def run():
 
     pdf_path = os.path.join(output_path, pdf_name + '.pdf')
 
-    print("Running " + pdf_path + " in Chrome browser")
+    print("Running " + pdf_path + " in Chrome browser and opening folder:" + output_path)
     webbrowser.open('file://' + os.path.realpath(pdf_path))
+    webbrowser.open(os.path.realpath(output_path))
+
+    default_backup_output_dir = "write default_backup_output_dir"
+    
+    backup_output_dir = input("Enter backup output directory path (press enter to use default directory):" + "\n"
+                    + default_output_dir  + "\n" )
+    if backup_output_dir == "":
+        backup_output_dir = default_backup_output_dir
+        print("Chosen Output Default Directory:" + "\n" + output_dir + "\\" + pdf_name);
+
+# Create a backup of the PDF file
+    if backup_output_dir != "":
+        backup_path = os.path.join(backup_output_dir, pdf_name)
+    if not os.path.exists(backup_path):
+        os.makedirs(backup_path)
+    backup_file = os.path.join(backup_path, pdf_name + '_backup.pdf')
+    shutil.copyfile(pdf_path, backup_file)
+    print("Backup file saved to " + backup_file)
 
 
 
